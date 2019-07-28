d01b22119b385f403c3dfadfc2b4e4471c17df87
Ju Garcia <julianagarcia2009@gmail.com>
2019-07-28 16:16:07 -0300

    adiciona carousel

--- js/training.js
+++ js/training.js
@@ -1,15 +1,15 @@
-const obj = {
-	outer: function() {
-        const self1 = this
+// const obj = {
+// 	outer: function() {
+//         const self1 = this
 
-        const inner1 = () => {
-            const self2 = this
+//         const inner1 = () => {
+//             const self2 = this
 
-            const inner2 = () => {
-                const self3 = this
-            }
-        }
-    }
-}
+//             const inner2 = () => {
+//                 const self3 = this
+//             }
+//         }
+//     }
+// }
 
-console.log(obj.outer(this.inner1 === this.inner2));
+// console.log(obj.outer(this.inner1 === this.inner2));
